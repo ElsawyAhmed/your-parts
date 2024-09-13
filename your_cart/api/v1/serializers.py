3d171cb32d49957ab1e5fd2cde2239f0be088f96
ElsawyAhmed <sawyahmed951@gmail.com>
2024-09-13 19:05:26 +0300

    serializers skeleton

---
+++ your_cart/api/v1/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    pass
+
+
+class CartSerializer(serializers.ModelSerializer):
+    pass
+
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    pass
+
+
+
+class StockSerializer(serializers.ModelSerializer):
+    pass
+
